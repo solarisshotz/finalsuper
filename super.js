1f051d7882d6244310af1c29c9962e3d45679d6c
kmarshall2 <45017660+kmarshall2@users.noreply.github.com>
2019-11-22 13:47:24 -0800

    Add files via upload

--- super.js
+++ super.js
@@ -19,41 +19,50 @@ var m= document.getElementById("fortlogo")
 function resetColumn1 () {
 	e.classList.add("hidebox")
 	f.classList.add("hidebox")
+	a.classList.toggle("pulse1");
 }
 
 function resetColumn2 () {
 	g.classList.add("hidebox")
 	h.classList.add("hidebox")
+	b.classList.toggle("pulse1");
 }
 
 function resetColumn3 () {
 	i.classList.add("hidebox")
-	j.classList.add("hidebox")	
+	j.classList.add("hidebox")
+	c.classList.toggle("pulse1");	
 }
 
 function addPics () {
 	e.classList.remove("hidebox");
 	f.classList.remove("hidebox");
+	a.classList.remove("pulse1");
 }
 
 function addPics1 () {
 	g.classList.remove("hidebox");
 	h.classList.remove("hidebox");
+	b.classList.remove("pulse1");
 }
 
 function addPics2 () {
 	i.classList.remove("hidebox");
 	j.classList.remove("hidebox");
+	c.classList.remove("pulse1");
 }
 
 function addr6Info () {
 	e.classList.toggle("r6info")
+	e.classList.toggle("pulse1");
 }
 
 function addrlInfo () {
 	g.classList.toggle("rlinfo")
+	g.classList.toggle("pulse1");
 }
 
 function addfortInfo () {
 	i.classList.toggle("fortinfo")
+	i.classList.toggle("pulse1");
 }
